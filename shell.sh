0aef1916adfb89c22e740f2cae8993f0764e18c8
Antoine Benevaut <antoine@cavaencoreparlerdebits.fr>
2015-08-30 17:55:25 +0200

    Update debian packages installation.

--- shell.sh
+++ shell.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
 echo "Processing server installation"
+
 sudo apt-get update -y -qq > /dev/null
 sudo apt-get upgrade -y -qq > /dev/null
+
+# i386 packages
 sudo dpkg --add-architecture i386 > /dev/null
+sudo apt-get update -y -qq > /dev/null
+sudo apt-get install libc6-i386 lib32gcc1 -y -qq > /dev/null
+
+# i386 packages
+sudo dpkg --add-architecture i386 > /dev/null
+sudo apt-get update -y -qq > /dev/null
+sudo aptitude install ia32-libs -y -q=9 > /dev/null
+
 sudo apt-get install gdb tmux expect -y -qq > /dev/null
+
+# Force dependencies
 sudo apt-get install -f -y -qq
 
 echo "Install CS:GO server"
